10b5ddf745e2bce893d0ba49cd0e7d9b4a03d6be
Adam Walter <adam@vtldesign.com>
2021-12-16 14:54:17 -0500

    Fixes syntax deprecated in PHP 8

--- view/sendgrid_settings_test_email.php
+++ view/sendgrid_settings_test_email.php
@@ -13,31 +13,31 @@
         <tbody>
           <tr valign="top">
             <td colspan="2">
-              <h3><?php echo _e('SendGrid Test - Send a test email with these settings') ?></h3>
+              <h3><?php echo _e('SendGrid Test - Send a test email with these settings'); ?></h3>
             </td>
           </tr>
           <tr valign="top">
             <th scope="row"><?php _e("To: "); ?></th>
             <td>
-              <input type="text" name="sendgrid_to" required="true" value="<?php echo isset( $success ) ? '' : isset( $to ) ? $to : '' ; ?>" size="20" class="regular-text">
+              <input type="text" name="sendgrid_to" required="true" value="<?php echo isset( $success ) ? '' : (isset( $to ) ? $to : ''); ?>" size="20" class="regular-text">
             </td>
           </tr>
           <tr valign="top">
             <th scope="row"><?php _e("Subject: "); ?></th>
             <td>
-              <input type="text" name="sendgrid_subj" required="true" value="<?php echo isset( $success ) ? '' : isset( $subject ) ? $subject : '' ; ?>" size="20" class="regular-text">
+              <input type="text" name="sendgrid_subj" required="true" value="<?php echo isset( $success ) ? '' : (isset( $subject ) ? $subject : ''); ?>" size="20" class="regular-text">
             </td>
           </tr>
           <tr valign="top">
             <th scope="row"><?php _e("Body: "); ?></th>
             <td>
-              <textarea name="sendgrid_body" rows="5" class="large-text"><?php echo isset( $success ) ? '' : isset( $body ) ? $body : '' ; ?></textarea>
+              <textarea name="sendgrid_body" rows="5" class="large-text"><?php echo isset( $success ) ? '' : (isset( $body ) ? $body : ''); ?></textarea>
             </td>
           </tr>
           <tr valign="top">
             <th scope="row"><?php _e("Headers: "); ?></th>
             <td>
-              <textarea name="sendgrid_headers" rows="3" class="large-text"><?php echo isset( $success ) ? '' : isset( $headers ) ? $headers : ''; ?></textarea>
+              <textarea name="sendgrid_headers" rows="3" class="large-text"><?php echo isset( $success ) ? '' : (isset( $headers ) ? $headers : ''); ?></textarea>
             </td>
           </tr>
         </table>
@@ -45,7 +45,7 @@
       <input type="hidden" name="email_test" value="true"/>
       <input type="hidden" name="sgnonce" value="<?php echo wp_create_nonce('sgnonce'); ?>"/>
       <p class="submit">
-        <input class="button button-primary" type="submit" name="Submit" value="<?php _e('Send') ?>" />
+        <input class="button button-primary" type="submit" name="Submit" value="<?php _e('Send'); ?>" />
       </p>
     </form>
   <?php endif; ?>
